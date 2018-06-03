1434921f5515a59558be1f487d2ce1861bc10164
Dan <DanDudemeister@users.noreply.github.com>
2018-06-03 11:33:47 +0200

    initial commit

---
+++ src/cleancode/testcases/nullreturn/TestCalls.java
@@ -0,0 +1,63 @@
+package cleancode.testcases.nullreturn;
+
+import java.util.Date;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+public class TestCalls {
+
+    private static final String DATE_AS_STRING = "2018-06-03";
+    private TestMethods testMethods;
+
+    public TestCalls() {
+        testMethods = new TestMethods();
+    }
+
+
+    private void callMethods() {
+
+        // assignment
+        Date date = testMethods.createDateFromString(DATE_AS_STRING);
+
+
+        // chain call
+        long time = testMethods.createDateFromString(DATE_AS_STRING).getTime();
+
+
+        // chain call in different lines
+        time = testMethods
+                .createDateFromString(DATE_AS_STRING)
+                .getTime();
+
+
+        // already adapted
+        Optional<Date> optionalDate1 = testMethods.createDateFromStringAlreadyAdapted(DATE_AS_STRING);
+
+
+        // methodreference
+        BiFunction<TestMethods, String, Date> createDateFromString1 = TestMethods::createDateFromString;
+
+
+        // lambda
+        BiFunction<TestMethods, String, Long> createDateFromString2 =
+                (testMethods1, dateAsString) -> testMethods1.createDateFromString(dateAsString).getTime();
+
+
+        // if
+        if (testMethods.createDateFromString(DATE_AS_STRING) != null) {
+            System.out.println("");
+        }
+
+
+        // non-generic Optional
+        Optional optionalDate2 = testMethods.createDateFromStringAlreadyAdapted(DATE_AS_STRING);
+
+
+        // method call inherited from object
+        String string = testMethods.createDateFromString(DATE_AS_STRING).toString();
+
+
+        // method call without any context
+        testMethods.createDateFromString(DATE_AS_STRING);
+    }
+}
---
+++ src/cleancode/testcases/nullreturn/TestMethods.java
@@ -0,0 +1,128 @@
+package cleancode.testcases.nullreturn;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class TestMethods {
+
+    // wrapper classes
+
+    public Integer getAnInteger() {
+        return null;
+    }
+
+
+    // string
+
+    public String getAString() {
+        return null;
+    }
+
+
+    // collections
+
+    public List<String> getAList() {
+        return null;
+    }
+
+
+    public Map<String, String> getAMap() {
+        return null;
+    }
+
+
+    public Set<String> getASet() {
+        return null;
+    }
+
+
+    public Iterator<String> getAnIterator() {
+        return null;
+    }
+
+
+    // arrays
+
+    public String[] getAnArray() {
+        return null;
+    }
+
+
+    public String[][] getATwoDimensionalArray() {
+        return null;
+    }
+
+
+    public String[][][] getAThreeDimensionalArray() {
+        return null;
+    }
+
+
+    // other types
+
+    public Date createDateFromString(String dateAsString) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            return formatter.parse(dateAsString);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+
+    // return type already adapted
+
+    public Optional<Date> createDateFromStringAlreadyAdapted(String dateAsString) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            return Optional.ofNullable(formatter.parse(dateAsString));
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+
+    // declaration
+
+    public String getAStringDeclaredWithNull() {
+        String string = null;
+        return string;
+    }
+
+
+    // multiple declarations at once
+
+    public String getAStringMultipleDeclarations() {
+        String a, b, c;
+        b = null;
+        return b;
+    }
+
+
+    // assignment
+
+    public String getAStringLaterAssignedAsNull() {
+        String string;
+        string = null;
+        return string;
+    }
+
+
+    // multiple assignments at once
+
+    public String getAStringMultipleAssignments() {
+        String a, b, c;
+        a = b = c = null;
+        return b;
+    }
+
+
+    // ternary Expression
+
+    public String getAStringWithTernaryOperator(boolean bool) {
+        return bool ? "true" : null;
+    }
+}
