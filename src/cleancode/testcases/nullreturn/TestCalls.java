377401a6ee8654bf48585185544ec0aca694d4ab
Dan <DanDudemeister@users.noreply.github.com>
2018-06-03 15:11:05 +0200

    some minor changes

--- src/cleancode/testcases/nullreturn/TestCalls.java
+++ src/cleancode/testcases/nullreturn/TestCalls.java
@@ -13,49 +13,39 @@ public class TestCalls {
         testMethods = new TestMethods();
     }
 
-
     private void callMethods() {
 
         // assignment
         Date date = testMethods.createDateFromString(DATE_AS_STRING);
 
-
         // chain call
         long time = testMethods.createDateFromString(DATE_AS_STRING).getTime();
-
 
         // chain call in different lines
         time = testMethods
                 .createDateFromString(DATE_AS_STRING)
                 .getTime();
 
-
         // already adapted
         Optional<Date> optionalDate1 = testMethods.createDateFromStringAlreadyRefactored(DATE_AS_STRING);
-
 
         // method reference
         BiFunction<TestMethods, String, Date> createDateFromString1 = TestMethods::createDateFromString;
 
-
         // lambda
         BiFunction<TestMethods, String, Long> createDateFromString2 =
                 (testMethods1, dateAsString) -> testMethods1.createDateFromString(dateAsString).getTime();
-
 
         // if
         if (testMethods.createDateFromString(DATE_AS_STRING) != null) {
             System.out.println("");
         }
 
-
         // non-generic Optional
         Optional optionalDate2 = testMethods.createDateFromStringAlreadyRefactored(DATE_AS_STRING);
 
-
         // method call inherited from object
         String string = testMethods.createDateFromString(DATE_AS_STRING).toString();
-
 
         // method call without any context
         testMethods.createDateFromString(DATE_AS_STRING);
--- src/cleancode/testcases/nullreturn/TestMethods.java
+++ src/cleancode/testcases/nullreturn/TestMethods.java
@@ -7,60 +7,46 @@ import java.util.*;
 public class TestMethods {
 
     // wrapper classes
-
     public Integer getAnInteger() {
         return null;
     }
 
-
     // string
-
     public String getAString() {
         return null;
     }
 
-
     // collections
-
     public List<String> getAList() {
         return null;
     }
-
 
     public Map<String, String> getAMap() {
         return null;
     }
 
-
     public Set<String> getASet() {
         return null;
     }
-
 
     public Iterator<String> getAnIterator() {
         return null;
     }
 
-
     // arrays
-
     public String[] getAnArray() {
         return null;
     }
-
 
     public String[][] getATwoDimensionalArray() {
         return null;
     }
 
-
     public String[][][] getAThreeDimensionalArray() {
         return null;
     }
 
-
     // other types
-
     public Date createDateFromString(String dateAsString) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -71,60 +57,46 @@ public class TestMethods {
         }
     }
 
-
     // declaration
-
     public String getAStringDeclaredWithNull() {
         String string = null;
         return string;
     }
 
-
     // multiple declarations at once
-
     public String getAStringMultipleDeclarations() {
         String a, b, c;
         b = null;
         return b;
     }
 
-
     // assignment
-
     public String getAStringLaterAssignedAsNull() {
         String string;
         string = null;
         return string;
     }
 
-
     // multiple assignments at once
-
     public String getAStringMultipleAssignments() {
         String a, b, c;
         a = b = c = null;
         return b;
     }
 
-
     // ternary Expression
-
     public String getAStringWithTernaryOperator(boolean bool) {
         return bool ? "true" : null;
     }
 
-
     // already refactored
-
     public String getAStringAlreadyRefactored() {
         return "";
     }
 
-
     public List<String> getAListAlreadyRefactored() {
         return Collections.emptyList();
     }
-
 
     public Optional<Date> createDateFromStringAlreadyRefactored(String dateAsString) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
